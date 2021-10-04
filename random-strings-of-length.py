f1a69b1ea55b80ca337c2a7c3e52152c2109770a
J. Chris Wingeier II <52218669+jcwii@users.noreply.github.com>
2021-10-04 18:35:36 -0400

    Create random-strings-of-length.py

---
+++ random-strings-of-length.py
@@ -0,0 +1,8 @@
+import string
+import random
+
+def rsol(size=6, chars=string.ascii_lowercase + string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+def rsols(size=6, chars=string.ascii_lowercase + string.ascii_uppercase + string.digits + '~`! @#$%^&*()_-+={[}]|\:;"\'<,>.?/'):
+    return ''.join(random.choice(chars) for _ in range(size))
