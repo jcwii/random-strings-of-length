5b18611d7d6caaa16e690bee11f04058c3cb7314
J. Chris Wingeier II <52218669+jcwii@users.noreply.github.com>
2021-10-04 19:17:38 -0400

    readability

--- random-strings-of-length.py
+++ random-strings-of-length.py
@@ -1,5 +1,5 @@
-import string
 import random
+import string
 
 def rsol(size=6, chars=string.ascii_lowercase + string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
